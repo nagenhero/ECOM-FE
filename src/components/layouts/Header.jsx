24dde6016856d9dc41f68e739d173d5387a668a9
Nagendra Shrestha <nagen.671@gmail.com>
2025-04-07 09:38:02 +1000

    to be done logn

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -6,58 +6,79 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
 import { FcFactory } from "react-icons/fc";
-
+const isAuthenticated = true;
 export const Header = () => {
   return (
-    <Navbar expand="md" className="bg-dark  text-white" variant="dark">
+    <Navbar expand="md" className="navbar">
       <Container>
         <Navbar.Brand href="#home">
-          {" "}
           <FcFactory />
           <span> ECOMMERCE WEBSITE</span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Link className="nav-link" to="/">
+            <Link className="nav-link hover:underline" to="/">
               Home
             </Link>
-            {/* check for user._id */}
 
-            <>
-              {/* private menu */}
-              <Link className="nav-link" to="/dashboard">
-                Dashboard
-              </Link>
-              <Link
-                className="nav-link"
-                onClick={() => {
-                  // console.log("TEST");
-                  // DONE clear local and session storage add it to user Action
-                  localStorage.removeItem("refreshJWT");
-                  sessionStorage.removeItem("accessJWT");
-
-                  // reset user in store
-                  dispatch(resetUser());
-                  navigate("/");
-                }}
-              >
-                Logout
-              </Link>
-            </>
-
-            <>
-              {/* public menus */}
-              <Link className="nav-link" to="/signup">
-                Sign Up
-              </Link>
-              <Link className="nav-link" to="/signin">
-                Sign In
-              </Link>
-            </>
+            {/* Conditionally render based on authentication state */}
+            {isAuthenticated ? (
+              <>
+                <Link className="nav-link hover:underline" to="/dashboard">
+                  Dashboard
+                </Link>
+                <Link className="nav-link hover:underline">Logout</Link>
+              </>
+            ) : (
+              <>
+                <Link className="nav-link hover:underline" to="/signup">
+                  Sign Up
+                </Link>
+                <Link className="nav-link hover:underline" to="/signin">
+                  Sign In
+                </Link>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+
+    // <Navbar expand="md" className="bg-dark  text-white" variant="dark">
+    //   <Container>
+    //     <Navbar.Brand href="#home">
+    //       {" "}
+    //       <FcFactory />
+    //       <span> ECOMMERCE WEBSITE</span>
+    //     </Navbar.Brand>
+    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //     <Navbar.Collapse id="basic-navbar-nav">
+    //       <Nav className="ms-auto">
+    //         <Link className="nav-link" to="/">
+    //           Home
+    //         </Link>
+    //         {/* check for user._id */}
+
+    //         <>
+    //           {/* private menu */}
+    //           <Link className="nav-link" to="/dashboard">
+    //             Dashboard
+    //           </Link>
+    //         </>
+
+    //         <>
+    //           {/* public menus */}
+    //           <Link className="nav-link" to="/signup">
+    //             Sign Up
+    //           </Link>
+    //           <Link className="nav-link" to="/signin">
+    //             Sign In
+    //           </Link>
+    //         </>
+    //       </Nav>
+    //     </Navbar.Collapse>
+    //   </Container>
+    // </Navbar>
   );
 };
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,38 +1,20 @@
 import React from "react";
 import img1 from "../../assets/o.jpg";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { FcFactory } from "react-icons/fc";
+import { Link, useNavigate } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      <div style={{ position: "relative" }}>
-        <img
-          src={img1}
-          alt="My Image"
-          style={{ width: "100%" }}
-          className=" filter blur-sm"
-        />
-        <div
-          style={{
-            position: "absolute",
-            top: "0",
-            left: "0",
-            width: "100%",
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            textAlign: "center",
-          }}
-        >
-          <h1 className="text-8xl font-bold text-white mb-4">
-            Welcome to Our Store
-          </h1>
-          <p className="text-8xl font-bold text-white mb-6">
-            Your one-stop shop for the best products online.
-          </p>
-        </div>
-      </div>
+    <div
+      style={{
+        backgroundImage: `url(${img1})`,
+        backgroundSize: "cover",
+        minHeight: "93vh",
+        marginTop: "-60px",
+      }}
+    >
+      {/* Page content goes here */}
     </div>
   );
 };
